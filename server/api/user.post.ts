d410c82d1257d9330d6d7bf05b1bad94a76420c3
Clemy-beep <clementine.digny@gmail.com>
2022-10-27 18:37:45 +0200

    user creation

--- server/api/user.post.ts
+++ server/api/user.post.ts
@@ -4,8 +4,27 @@ const prisma = new PrismaClient();
 
 const createUser = defineEventHandler(async (event) => {
   const body = await readBody(event);
+  const name = body.name;
   const password = await bcrypt.hash(body.password, 10);
-  return {body, password};
+try {
+  await prisma.user.create({
+    data: {
+      name: name,
+      password: password,
+    }
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify("User created"),
+  };
+} catch (error) {
+  return {
+    statusCode: 500,
+    body: JSON.stringify({ message: "User could not be created" }),
+  }
+}
+  
+
 });
 
 export default createUser;
