fbe5e0b61216076a41451d3ea0999327d9440a7a
mortifia <amela57290@gmail.com>
2022-11-08 11:47:08 +0100

    update store and view fixed #17

--- stores/task.js
+++ stores/task.js
@@ -1,24 +1,21 @@
-import { defineStore, storeToRefs } from "pinia";
+import { defineStore, storeToRefs } from 'pinia'
 
-export const useTaskStore = defineStore("tasks", {
-    state: () => ({
-        tasks: storeToRefs([]),
-    }),
+export const useTaskStore = defineStore('tasks', {
+  state: () => ({
+    tasks: storeToRefs([]),
+  }),
 
-    actions: {
-        async setTasks() {
-            if (this.tasks.length > 0) return;
-            const res = await fetch("http://localhost:3000/api/task", {
-                    method: "GET",
-                })
-                .then((r) => r.json())
-                .catch((e) => {
-                    console.log(e);
-                });
-            if (res.statusCode === 200) {
-                this.tasks = res.body.tasks;
-            }
-        },
+  actions: {
+    async setTasks() {
+      const res = await fetch('http://localhost:3000/api/task', {
+        method: 'GET',
+      })
+        .then(r => r.json())
+        .catch(e => console.log('error', e))
+      if (res.statusCode === 200) {
+        this.tasks = res.body.tasks
+      }
     },
-    persist: true,
-});
+  },
+  persist: true,
+})
