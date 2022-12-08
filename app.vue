12663dc2855ac61a96a8a3c9ee1bd7e250a134ff
Killian <k.wauters@tribu-creative.io>
2022-12-08 15:09:07 +0100

    Bouton retourner en haut de la page Fixes #28

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="body">
-    <Header @login="show" />
+    <Header @login="show" id="haut"/>
     <LoginForm :show-form="showForm" @close="closeForm" />
     <TaskList />
     <AddTaskForm />
+    <a href="#haut"><img id="cRetour" src="./Images/arrow_up.png" alt="up"></a>
+    <!-- <a id="cRetour" href="#haut">HAUT</a> -->
   </div>
 </template>
 
@@ -28,5 +30,10 @@ body {
   min-height: 100vh;
   background-color: #171e27;
 
+}
+#cRetour{
+  position: fixed;
+  bottom: 20px;
+  right: 50px;
 }
 </style>
