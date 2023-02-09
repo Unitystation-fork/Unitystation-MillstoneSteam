74ad4f70c9df324e19c196e28249593893659439
Clemy-beep <clementine.digny@gmail.com>
2023-02-09 14:04:27 +0100

    taches completees rassemblees en haut de la liste

--- stores/task.js
+++ stores/task.js
@@ -1,27 +1,37 @@
 import { defineStore, storeToRefs } from "pinia";
 
 export const useTaskStore = defineStore("tasks", {
-    state: () => ({
-        tasks: storeToRefs([]),
-    }),
+  state: () => ({
+    tasks: storeToRefs([]),
+  }),
 
-    actions: {
-        async setTasks() {
-            const res = await fetch("http://localhost:3000/api/task", {
-                    method: "GET",
-                })
-                .then((r) => r.json())
-                .catch((e) => {
-                    console.log("error", e);
-                    return false;
-                });
-            if (res.statusCode === 500) {
-                console.log("error", res);
-                return false;
-            }
-            this.tasks = res.body.tasks;
-            return true;
-        },
+  actions: {
+    async setTasks() {
+      const res = await fetch("http://localhost:3000/api/task", {
+        method: "GET",
+      })
+        .then((r) => r.json())
+        .catch((e) => {
+          console.log("error", e);
+          return false;
+        });
+      if (res.statusCode === 500) {
+        console.log("error", res);
+        return false;
+      }
+      //sort tasks by status
+
+      this.tasks = res.body.tasks.sort((a, b) => {
+        if (a.completed < b.completed) {
+          return 1;
+        }
+        if (a.completed > b.completed) {
+          return -1;
+        }
+        return 0;
+      });
+      return true;
     },
-    persist: true,
+  },
+  persist: true,
 });
