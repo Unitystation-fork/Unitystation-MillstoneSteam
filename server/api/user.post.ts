44eadff977326d07cff9e575466bf518614f74bb
Younes Bourakadi <younes.bourakadi@gmail.com>
2023-10-06 15:51:32 +0200

    Update user.post.ts to reflect schema change

--- server/api/user.post.ts
+++ server/api/user.post.ts
@@ -4,68 +4,64 @@ import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
 const createUser = defineEventHandler(async (event) => {
-  // get the body of the request and asign its parameters to variables
   const body = await readBody(event);
   const name = body.name;
   const role = body.role;
   const password = await bcrypt.hash(body.password, 10);
+  const discordId = body.discordId;
+  const twitchId = body.twitchId;
   const auth = event.req.headers.authorization;
-  const token = (auth !== undefined) ?  auth.split(" ")[1] : null;
-try {
-  // verify the token validity
-  if (!token) {
-    throw "No token provided";
-  }
-  const decoded: string | JwtPayload = jwt.verify(token, process.env.JWT_SECRET);
-  if (!decoded || typeof decoded === "string") {
-    throw "Invalid token";
-  }
-  // check if the user exists and throw an error if it doesn't
-  const user = await prisma.user.findUnique({
-    where: {
-      id: decoded.id,
+  const token = (auth !== undefined) ? auth.split(" ")[1] : null;
+  try {
+    // vérifiez la validité du jeton
+    if (!token) {
+      throw "No token provided";
     }
-  });
-  if (!user) {
-    throw "User not found";
-  }
-  //check if user is admin
-  if (user.role !== "ADMIN") {
-    throw "You do not have the permission to create users";
-  }
-  // check required infos are present
-  if(!name || !password) {
-    throw "Name or password missing";
-  }
-  // check if user already exists
-  const userToCreate = await prisma.user.findUnique({
-    where: {
-      name: name, 
+    const decoded: string | JwtPayload = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded || typeof decoded === "string") {
+      throw "Invalid token";
     }
-  });
-  if(userToCreate) {
-    throw "User already exists";
-  }
-    // create the user with provided name and password
-  const createdUser = await prisma.user.create({
-    data: {
-      name: name,
-      password: password,
-      role: role,
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decoded.id,
+      }
+    });
+    if (!user) {
+      throw "User not found";
     }
-  });
-  return {
-    statusCode: 200,
-    body: {message: "User created", user : createdUser },
-  };
-} catch (error) {
-  return {
-    statusCode: 500,
-    body:{ message: "User could not be created", error: error },
+    if (user.role !== "ADMIN") {
+      throw "You do not have permission to create users";
+    }
+    if (!name || !password) {
+      throw "Name or password missing";
+    }
+    const userToCreate = await prisma.user.findUnique({
+      where: {
+        name: name,
+      }
+    });
+    if (userToCreate) {
+      throw "User already exists";
+    }
+    const createdUser = await prisma.user.create({
+      data: {
+        name: name,
+        password: password,
+        role: role,
+        discordId: discordId,
+        twitchId: twitchId,
+      }
+    });
+    return {
+      statusCode: 200,
+      body: { message: "User created", user: createdUser },
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: { message: "User could not be created", error: error },
+    };
   }
-}
-  
-
 });
 
 export default createUser;
